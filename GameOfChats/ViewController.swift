7610775ea7604ddcea6b0f8afe87b4d3c98ce420
omq78 <omar.qab@gmail.com>
2019-05-01 13:15:19 +0300

    Initial Commit

---
+++ GameOfChats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameOfChats
+//
+//  Created by Omar Alqabbani on 5/1/19.
+//  Copyright © 2019 OmarALqabbani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
